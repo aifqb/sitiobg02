ad71c513dbf19fdc9851bf9f30c5f8c856272e8a
aifqb <alexquisbert@gmail.com>
2021-07-24 12:49:00 -0400

    paso2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './share/header/header.component';
 import { FooterComponent } from './share/footer/footer.component';
+import { ServicesService } from './services/services.service';
 
 @NgModule({
   declarations: [
