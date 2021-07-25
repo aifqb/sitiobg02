2101a24b20390113ebf14c0fc3dc04d84d89ab7e
aifqb <alexquisbert@gmail.com>
2021-07-24 23:47:37 -0400

    material

--- src/app/productos/productos-routing.module.ts
+++ src/app/productos/productos-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { BuscarComponent } from './pages/buscar/buscar.component';
+import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
 import { ProductoComponent } from './pages/producto/producto.component';
 
 const routes: Routes = [
   {
     path: '',
+    component: HomeComponent,
     children: [
       { path: 'listado', component: ListadoComponent },
       { path: 'buscar', component: BuscarComponent },
--- src/app/productos/productos.module.ts
+++ src/app/productos/productos.module.ts
@@ -1,18 +1,33 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+
+import { ProductosRoutingModule } from './productos-routing.module';
+import { MaterialModule } from '../material/material.module';
+
 import { BuscarComponent } from './pages/buscar/buscar.component';
 import { ProductoComponent } from './pages/producto/producto.component';
 import { HomeComponent } from './pages/home/home.component';
 import { ListadoComponent } from './pages/listado/listado.component';
-import { ProductosRoutingModule } from './productos-routing.module';
+
+
 
 
 
 @NgModule({
-  declarations: [BuscarComponent, ProductoComponent, HomeComponent, ListadoComponent],
+  declarations:
+  [
+    BuscarComponent,
+    ProductoComponent,
+    HomeComponent,
+    ListadoComponent
+  ],
+    
   imports: [
     CommonModule,
-    ProductosRoutingModule
+    ProductosRoutingModule,
+    FlexLayoutModule,
+    MaterialModule
   ]
 })
 export class ProductosModule { }
