48ae32dab7e57ce85f681e5cb0d9fffaad3b1e12
aifqb <alexquisbert@gmail.com>
2021-07-25 23:22:36 -0400

    con productos y tarjetas

--- src/app/material/material.module.ts
+++ src/app/material/material.module.ts
@@ -1,11 +1,22 @@
 import { NgModule } from '@angular/core';
 import {MatSidenavModule} from '@angular/material/sidenav';
+import {MatCardModule} from '@angular/material/card';
+import {MatButtonModule} from '@angular/material/button';
+import {MatListModule} from '@angular/material/list';
+import {MatIconModule} from '@angular/material/icon';
+import {MatToolbarModule} from '@angular/material/toolbar';
+
 
 
 @NgModule({
   exports:
   [
-    MatSidenavModule
+    MatSidenavModule,
+    MatCardModule,
+    MatButtonModule,
+    MatListModule,
+    MatIconModule,
+    MatToolbarModule
   ]
 })
 export class MaterialModule { }
