d345aba0e6d8d54f7e67fc02a4dfc98a7994136e
aifqb <alexquisbert@gmail.com>
2021-07-30 08:17:10 -0400

    con inters

---
+++ src/app/intefaces/info-pagina.interface.ts
@@ -0,0 +1,21 @@
+
+export interface InfoPagina {
+    celular_alex?: number;
+    celular_paz?: number;
+    central?: number;
+    ciudad?: string;
+    depto?: string;
+    direccion?: string;
+    email?: string;
+    facebook: string;
+    instagram?: string;
+    interno_ventas?: number;
+    lema01?: string;
+    lema02?: string;
+    nombre?: string;
+    pais?: string;
+    tiktok?: string;
+    twitter?: string;
+    web?: string;
+    youtube?: string;
+  }
