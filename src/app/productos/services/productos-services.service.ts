d319e277741d42f7d1a28ed056dc4ce1d5aa22e2
aifqb <alexquisbert@gmail.com>
2021-08-20 23:59:04 -0400

    cors resuelto

--- src/app/productos/services/productos-services.service.ts
+++ src/app/productos/services/productos-services.service.ts
@@ -12,22 +12,14 @@ export class ProductosServicesService {
 
   getProductos(): Observable<Producto[]> {
     // return this.http.get<Producto[]>('https://angular-html-517d8-default-rtdb.firebaseio.com/productos.json')
-<<<<<<< HEAD
-    return this.http.get<Producto[]>('https://webapiproductos.azurewebsites.net/api/productos')
-=======
     return this.http.get<Producto[]>('https://localhost:44342/api/productos')
->>>>>>> adf58ee9f3f42786747fbed099cd241c4f8a4c23
   }
   
   getProductosPorId( id: string): Observable<Producto> {
     return this.http.get<Producto>(`https://angular-html-517d8-default-rtdb.firebaseio.com/productos/${ id }.json`) 
   }
   getSugerencias( termino: string ): Observable<Producto[]> { 
-<<<<<<< HEAD
-    return this.http.get<Producto[]>(`https://webapiproductos.azurewebsites.net/api/productos/producto?cantidadRegistrosPorPagina=100&ProductoNombre=${ termino }`) 
-=======
     return this.http.get<Producto[]>(`https://localhost:44342/api/productos/busqueda?cantidadRegistrosPorPagina=50&ProductoNombre=${ termino }`) 
->>>>>>> adf58ee9f3f42786747fbed099cd241c4f8a4c23
   }
 
 }
