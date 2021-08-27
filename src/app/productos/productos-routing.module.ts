6b8c0bda59f587f11407a9b839d1794b4bc75650
aifqb <alexquisbert@gmail.com>
2021-08-27 16:25:05 -0400

    con rutas de mapas sin copiar

--- src/app/productos/productos-routing.module.ts
+++ src/app/productos/productos-routing.module.ts
@@ -6,29 +6,23 @@ import { ListadoComponent } from './pages/listado/listado.component';
 import { ProductoComponent } from './pages/producto/producto.component';
 import { CategoriaComponent } from './pages/categoria/categoria.component';
 
-
 const routes: Routes = [
   {
     path: '',
     component: HomeComponent,
     children: [
-      { path: 'listado',    component: ListadoComponent },
-      { path: 'buscar',     component: BuscarComponent },
-      { path: 'categoria',  component: CategoriaComponent },
-      { path: ':id',        component: ProductoComponent },
-      { path: '**',         redirectTo: 'listado' }
-    ] 
-  }
+      { path: 'listado', component: ListadoComponent },
+      { path: 'buscar', component: BuscarComponent },
+      { path: 'categoria', component: CategoriaComponent },
+      { path: ':id', component: ProductoComponent },
+      { path: '**', redirectTo: 'listado' },
+    ],
+  },
 ];
-
 
 @NgModule({
   declarations: [],
-  imports: [
-    RouterModule.forChild(routes)
-  ],
-  exports: [
-    RouterModule
-  ]
+  imports: [RouterModule.forChild(routes)],
+  exports: [RouterModule],
 })
-export class ProductosRoutingModule { }
+export class ProductosRoutingModule {}
