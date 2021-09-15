a04a36bcde2636cde5e613e006c810003d5aa73d
alex francesc <76713839+aifqb@users.noreply.github.com>
2021-09-15 00:08:25 -0400

    con interarctividad

--- src/app/mapas/pages/full-screen/full-screen.component.ts
+++ src/app/mapas/pages/full-screen/full-screen.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  OnInit,
+  ViewChild,
+  ElementRef,
+  AfterViewInit,
+} from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 
 @Component({
@@ -6,20 +12,29 @@ import * as mapboxgl from 'mapbox-gl';
   templateUrl: './full-screen.component.html',
   styleUrls: ['./full-screen.component.css'],
 })
-export class FullScreenComponent implements OnInit {
+export class FullScreenComponent implements AfterViewInit {
+  @ViewChild('mapa') divMapa!: ElementRef;
+  mapa!: mapboxgl.Map;
   constructor() {}
 
-  ngOnInit(): void {
-    var map = new mapboxgl.Map({
-      container: 'mapa',
+  ngAfterViewInit(): void {
+    this.mapa = new mapboxgl.Map({
+      container: this.divMapa.nativeElement,
       style: 'mapbox://styles/alexqb/cktccupfp07ra17semcjfh4k5',
       center: [-68.1280372, -16.5028083],
       zoom: 10,
     });
+    this.mapa.on('click', ({ point }) => {
+      const features = this.mapa.queryRenderedFeatures(point, {
+        layers: ['Basicbg'],
+      });
+
+      if (!features.length) {
+        return;
+      }
+      const feature = features[0];
+    });
   }
 }
 
-
-
-
-// style: 'mapbox://styles/mapbox/streets-v11',
+// https://angular-html-517d8-default-rtdb.firebaseio.com/pdv
