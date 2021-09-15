56bf0cfb0b0d05ecdafbff5c462ed850e4c8a654
alex francesc <76713839+aifqb@users.noreply.github.com>
2021-09-15 00:37:30 -0400

    con pop

--- src/app/mapas/pages/full-screen/full-screen.component.ts
+++ src/app/mapas/pages/full-screen/full-screen.component.ts
@@ -33,6 +33,12 @@ export class FullScreenComponent implements AfterViewInit {
         return;
       }
       const feature = features[0];
+      const popup = new mapboxgl.Popup({ offset: [0, -15] })
+        // .setLngLat(feature.geometry.coordinates)
+        // .setHTML(
+        //   `<h3>${feature.properties.title}</h3><p>${feature.properties.description}</p>`
+        // )
+        .addTo(this.mapa);
     });
   }
 }
