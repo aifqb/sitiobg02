a9c90243f0ecd12bbdd0974a329701c32254ccaa
alex francesc <76713839+aifqb@users.noreply.github.com>
2021-09-19 21:10:08 -0400

    pvlapaz

--- src/app/mapas/pages/propiedades/propiedades.component.ts
+++ src/app/mapas/pages/propiedades/propiedades.component.ts
@@ -1,12 +1,6 @@
 import { Component, ElementRef, ViewChild, AfterViewInit } from '@angular/core';
 import * as mapboxgl from 'mapbox-gl';
 import { Puntoventa } from '../../interfaces/puntos.interface';
-import { puntosService } from '../../services/puntos.service';
-
-interface Depto{
-  departamento: string;
-  }
-  
 
 @Component({
   selector: 'app-propiedades',
@@ -14,57 +8,29 @@ interface Depto{
   styleUrls: ['./propiedades.component.css'],
 })
 export class PropiedadesComponent implements AfterViewInit {
-
-  departamentos: Depto[] = [
-    { departamento: 'LaPaz'},
-    { departamento: 'Oruro'},
-    { departamento: 'Cochabamba'},
-    // { departamento: 'Potosi'},
-    // { departamento: 'Chuquisaca'},
-    // { departamento: 'Tarija'},
-    // { departamento: 'Santa Cruz'},
-    // { departamento: 'Beni'},
-    // { departamento: 'Pando'},
-  ];
-  selectedDepto = this.departamentos[0].departamento;
-
-  regionActiva: string = ''
-
   @ViewChild('mapa') divMapa!: ElementRef;
   mapa!: mapboxgl.Map;
   zoomLevel: number = 12;
-  LaPaz: [number, number] = [-68.1280372, -16.5028083];
-  Cochabamba: [number, number] = [-68.191235, -16.55043];
-  Oruro: [number, number] = [-67.109276, -17.966365];
+  center: [number, number] = [-68.121970, -16.501737];
+
 
   // Arreglo de marcadores
   marcadores: mapboxgl.Marker[] = [];
   puntos: Puntoventa[] = [];
 
-  constructor(private puntosVenta: puntosService) {}
+  constructor() {}
 
   ngAfterViewInit(): void {
     this.mapa = new mapboxgl.Map({
       container: this.divMapa.nativeElement,
       style: 'mapbox://styles/alexqb/cktccupfp07ra17semcjfh4k5',
-      center: this.LaPaz,
+      center: this.center,
       zoom: this.zoomLevel,
     });
 
-    this.puntosVenta.getPuntos().subscribe((puntos) => (this.puntos = puntos));
+    const marker = new mapboxgl.Marker()
+      .setLngLat(this.center)
+      .addTo(this.mapa);
 
-    const marker = new mapboxgl.Marker().setLngLat(this.LaPaz).addTo(this.mapa);
-    const marker2 = new mapboxgl.Marker().setLngLat(this.Cochabamba).addTo(this.mapa);
-    const marker3 = new mapboxgl.Marker().setLngLat(this.Oruro).addTo(this.mapa);
-
-  }
-
-  irMarcador(marker: mapboxgl.Marker) {
-    this.mapa.flyTo({
-      center: marker.getLngLat(),
-    });
-  }
-  activarRegion(option: string) {
-    this.regionActiva = option;
   }
 }
