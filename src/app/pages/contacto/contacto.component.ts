05cd626ea8f8312fe516c837dbc6938650688ab0
aifqb <alexquisbert@gmail.com>
2021-09-20 18:33:25 -0400

    retoques de pagina

--- src/app/pages/contacto/contacto.component.ts
+++ src/app/pages/contacto/contacto.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { InfoPaginaService } from 'src/app/services/info-pagina.service';
 
 @Component({
   selector: 'app-contacto',
@@ -7,7 +8,8 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ContactoComponent implements OnInit {
 
-  constructor() { }
+ 
+  constructor( public _servicio: InfoPaginaService ) { }
 
   ngOnInit(): void {
   }
