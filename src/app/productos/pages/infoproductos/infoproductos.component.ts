6f9bee13a7a3b949da69ac80892deba21ff4daf0
alex francesc <76713839+aifqb@users.noreply.github.com>
2022-06-14 13:58:28 -0400

    con api de firebase productos

--- src/app/productos/pages/infoproductos/infoproductos.component.ts
+++ src/app/productos/pages/infoproductos/infoproductos.component.ts
@@ -1,10 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { InfoContactoService } from 'src/app/services/info-contacto.service';
 
 export interface Sanjuan {
   "codigodeItem":         number;
   "nombreProducto":       string;
   "unidades":             number;
-  "pesoGr.":              number;
+  "pesoGr":              number;
   "PrecioCalc":           number;
   "PrecioFinal":          number;
   "tamanoCm":             number;
@@ -17,150 +18,16 @@ export interface Sanjuan {
   templateUrl: './infoproductos.component.html',
   styleUrls: ['./infoproductos.component.css']
 })
-export class InfoproductosComponent {
+export class InfoproductosComponent implements OnInit{
 
-  productos: Sanjuan[] = [
-    {
-     "codigodeItem": 38,
-     "nombreProducto": "Salchicha sin piel viena de 22 cm - a granel",
-     "unidades": 16,
-     "pesoGr.": 1000,
-     "PrecioCalc": 25,
-     "PrecioFinal": 27.5,
-     "tamanoCm": 22,
-     "vigente": "true"
-    },
-    {
-     "codigodeItem": 39,
-     "nombreProducto": "Salchicha sin piel viena de 22 cm - envasado 1 kg",
-     "unidades": 16,
-     "pesoGr.": 1000,
-     "PrecioCalc": 25,
-     "PrecioFinal": 28,
-     "tamanoCm": 22,
-     "vigente": "true"
-    },
-    {
-     "codigodeItem": 97,
-     "nombreProducto": "Salchicha sin piel viena de 22 cm - envasado 500 gr",
-     "unidades": 8,
-     "pesoGr.": 500,
-     "PrecioCalc": 12.5,
-     "PrecioFinal": 16,
-     "tamanoCm": 22,
-     "vigente": "true"
-    },
-    {
-     "codigodeItem": 172,
-     "nombreProducto": "Salchicha sin piel viena de 22 cm - sachet 800 gr",
-     "unidades": 12,
-     "pesoGr.": 780,
-     "PrecioCalc": 19.5,
-     "PrecioFinal": 23,
-     "tamanoCm": 22,
-     "vigente": "true"
-    },
-    {
-     "codigodeItem": 119,
-     "nombreProducto": "Salchicha sin piel viena de 17 cm - a granel",
-     "unidades": 21,
-     "pesoGr.": 1000,
-     "PrecioCalc": 25,
-     "PrecioFinal": 28,
-     "tamanoCm": 17,
-     "vigente": "true"
-    },
-    {
-     "codigodeItem": 108,
-     "nombreProducto": "Salchicha sin piel viena de 17 cm - envasado 1 kg",
-     "unidades": 21,
-     "pesoGr.": 1000,
-     "PrecioCalc": 25,
-     "PrecioFinal": 28,
-     "tamanoCm": 17,
-     "vigente": "true"
-    },
-    {
-     "codigodeItem": 112,
-     "nombreProducto": "Salchicha sin piel viena de 14 cm - envasado 1 kg",
-     "unidades": 22,
-     "pesoGr.": 1000,
-     "PrecioCalc": 25,
-     "PrecioFinal": 28,
-     "tamanoCm": 14,
-     "vigente": "true"
-    },
-    {
-     "codigodeItem": 113,
-     "nombreProducto": "Salchicha sin piel viena de 14 cm - envasado 500 gr",
-     "unidades": 12,
-     "pesoGr.": 500,
-     "PrecioCalc": 12.5,
-     "PrecioFinal": 16,
-     "tamanoCm": 14,
-     "vigente": "true"
-    },
-    {
-     "codigodeItem": 165,
-     "nombreProducto": "Salchicha sin piel viena de 14 cm - sachet 250 gr",
-     "unidades": 6,
-     "pesoGr.": 250,
-     "PrecioCalc": 6.3,
-     "PrecioFinal": 9.5,
-     "tamanoCm": 14,
-     "vigente": "true"
-    },
-    {
-     "codigodeItem": 73,
-     "nombreProducto": "Salchicha con piel pop - a granel",
-     "unidades": 26,
-     "pesoGr.": 1000,
-     "PrecioCalc": 25,
-     "PrecioFinal": 27,
-     "tamanoCm": 12,
-     "vigente": "true"
-    },
-    {
-     "codigodeItem": 137,
-     "nombreProducto": "Salchicha con piel pink - a granel",
-     "unidades": 26,
-     "pesoGr.": 1000,
-     "PrecioCalc": 25,
-     "PrecioFinal": 27,
-     "tamanoCm": 12,
-     "vigente": "true"
-    },
-    {
-     "codigodeItem": 43,
-     "nombreProducto": "Salchicha con piel viena - a granel",
-     "unidades": 26,
-     "pesoGr.": 1000,
-     "PrecioCalc": 25,
-     "PrecioFinal": 27,
-     "tamanoCm": 12,
-     "vigente": "true"
-    },
-    {
-     "codigodeItem": 44,
-     "nombreProducto": "Salchicha con piel viena ahumada - a granel",
-     "unidades": 26,
-     "pesoGr.": 1000,
-     "PrecioCalc": 25,
-     "PrecioFinal": 27,
-     "tamanoCm": 12,
-     "vigente": "true"
-    },
-    {
-     "codigodeItem": 94,
-     "nombreProducto": "Salchicha sin piel viena de 10 cm - sachet 200 gr",
-     "unidades": 12,
-     "pesoGr.": 200,
-     "PrecioCalc": 5,
-     "PrecioFinal": 10,
-     "tamanoCm": 10,
-     "vigente": "true"
-    }
-  ]
+  productos: Sanjuan[] = [];
+
+  constructor( public promoServices: InfoContactoService ) { }
   
+  ngOnInit(): void {
+    this.promoServices.getPromo()
+      .subscribe( productos => this.productos = productos);
+  }
 
+ 
 }
--- src/app/services/info-contacto.service.ts
+++ src/app/services/info-contacto.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { InfoContacto } from '../intefaces/info-contacto.interface';
 import { Observable } from 'rxjs';
 import { Videos } from '../intefaces/info-video.interface';
+import { Sanjuan } from '../productos/pages/infoproductos/infoproductos.component';
 
 @Injectable({
   providedIn: 'root'
@@ -12,6 +13,8 @@ export class InfoContactoService {
   private apiUrl: string = 'https://angular-html-517d8-default-rtdb.firebaseio.com/info-contacto.json';
 
   private apivideosUrl: string = 'https://angular-html-517d8-default-rtdb.firebaseio.com/videos.json';
+
+  private apipsjUrl: string = 'https://angular-html-517d8-default-rtdb.firebaseio.com/promo.json';
 
 
   constructor( private http: HttpClient) { }
@@ -24,5 +27,10 @@ export class InfoContactoService {
   getVideos(): Observable<Videos[]>{
     const url = `${ this.apivideosUrl }`
     return this.http.get<Videos[]>(url)
+  }
+
+  getPromo(): Observable<Sanjuan[]>{
+    const url = `${ this.apipsjUrl }`
+    return this.http.get<Sanjuan[]>(url)
   }
 }
